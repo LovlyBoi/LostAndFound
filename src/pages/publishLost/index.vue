a6d2b53d243d4cb7edc72b9004298a86f409a818
LovlyBoi <728881636@qq.com>
2024-03-14 08:12:58 +0800

    style: 格式化

--- src/pages/publishLost/index.vue
+++ src/pages/publishLost/index.vue
@@ -1,18 +1,31 @@
 <template>
   <div class="container w-3/5 min-h-full m-auto">
-    <NSteps class="pt-10 ml-16" :current="(currentRef as number)" :status="currentStatus">
+    <NSteps
+      class="pt-10 ml-16"
+      :current="currentRef as number"
+      :status="currentStatus"
+    >
       <NStep title="选择需求类型" description="Lost or Found" />
       <NStep title="填写基本信息" description="Basic Message" />
       <NStep title="预览发布信息" description="Preview the Message" />
     </NSteps>
     <div v-if="currentRef === 1" class="type-group text-center mt-20">
-      <NRadioGroup class="type-btn" v-model:value="type" name="radiobuttongroup1">
+      <NRadioGroup
+        class="type-btn"
+        v-model:value="type"
+        name="radiobuttongroup1"
+      >
         <NRadioButton class="text-2xl text-center h-8 mr-20" value="1">
           <SeekIcon font-size="120px" class="m-auto mt-5" />
           寻物
         </NRadioButton>
       </NRadioGroup>
-      <NRadioGroup class="type-btn" v-if="currentRef === 1" v-model:value="type" name="radiobuttongroup1">
+      <NRadioGroup
+        class="type-btn"
+        v-if="currentRef === 1"
+        v-model:value="type"
+        name="radiobuttongroup1"
+      >
         <NRadioButton class="text-2xl text-center h-8 mr-20" value="2">
           <FoundIcon font-size="120px" class="m-auto mt-5" />
           招领
@@ -22,30 +35,38 @@
     <div v-if="currentRef === 2" class="flex items-center justify-center pt-10">
       <BasicMessage :modelRef="modelRef" ref="basicInfoRef" />
     </div>
-    <div v-if="currentRef === 3" class="overflow-auto" style="max-height: calc(100vh - 200px);">
+    <div
+      v-if="currentRef === 3"
+      class="overflow-auto"
+      style="max-height: calc(100vh - 200px)"
+    >
       <Preview :modelRef="modelRef" :type="type" />
     </div>
-    <NSpace class="absolute bottom-6 mr-20" style="left:43%">
-      <NButton @click="prev" :disabled="currentRef === 1">
-        上一步
-      </NButton>
+    <NSpace class="absolute bottom-6 mr-20" style="left: 43%">
+      <NButton @click="prev" :disabled="currentRef === 1"> 上一步 </NButton>
       <NButton @click="next" v-if="currentRef && currentRef < 3">
         下一步
       </NButton>
-      <NButton @click="submit" v-if="currentRef === 3">
-        提 交
-      </NButton>
+      <NButton @click="submit" v-if="currentRef === 3"> 提 交 </NButton>
     </NSpace>
   </div>
-
 </template>
 
 <script setup lang="ts">
-import { NButton, NRadioButton, NRadioGroup, NSpace, NStep, NSteps, StepsProps, useMessage } from 'naive-ui';
+import {
+  NButton,
+  NRadioButton,
+  NRadioGroup,
+  NSpace,
+  NStep,
+  NSteps,
+  StepsProps,
+  useMessage,
+} from 'naive-ui'
 import { ref } from 'vue'
 import SeekIcon from '@/pages/publishLost/icons/SeekIcon.vue'
 import FoundIcon from '@/pages/publishLost/icons/FoundIcon.vue'
-import { ModelType } from './type';
+import { ModelType } from './type'
 import BasicMessage from './components/BasicMessage.vue'
 import Preview from './components/Preview.vue'
 
@@ -66,14 +87,13 @@ const modelRef = ref<ModelType>({
 
 const next = () => {
   if (currentRef?.value === null) {
-    currentRef.value = 1;
-    return;
+    currentRef.value = 1
+    return
   }
   if (currentRef.value === 1) {
     if (!type.value) {
       message.info('请选择您想要发布的类型')
-    }
-    else currentRef.value++;
+    } else currentRef.value++
   }
   if (currentRef.value === 2) {
     basicInfoRef.value?.formRef?.validate((errors: any) => {
@@ -95,7 +115,6 @@ const submit = () => {
   console.log({ type: type.value, data: modelRef.value })
   message.success('提交成功')
 }
-
 </script>
 
 <style scoped>
